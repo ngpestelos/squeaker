79ee4f83ea9429cf94af89b7222818732053e341
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2014-04-20 16:18:57 +0800

    add a view to show a message

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -6,3 +6,8 @@ Given /^the User has posted the message "([^"]*)"$/ do |message_text|
   expect(User.count).to equal 1
   FactoryGirl.create(:message, content: message_text, user: User.first)
 end
+
+When /^I visit the page for this User$/ do
+  expect(User.count).to equal 1
+  visit(user_path(User.first))
+end
