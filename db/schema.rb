310553b49f158e485921762ae3d9e2a8b134d200
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2014-04-20 15:57:52 +0800

    add Message model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420073039) do
+ActiveRecord::Schema.define(version: 20140420075102) do
+
+  create_table "messages", force: true do |t|
+    t.integer  "user_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,8 @@
 Given /^there is a User$/ do
   FactoryGirl.create(:user)
 end
+
+Given /^the User has posted the message "([^"]*)"$/ do |message_text|
+  expect(User.count).to equal 1
+  FactoryGirl.create(:message, content: message_text, user: User.first)
+end
